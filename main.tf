ad590a59c9381aaa548b6ff83a8c61d9337f1bd1
tony <tony@xiatech.co.uk>
2022-02-15 11:19:37 +0100

    working

--- main.tf
+++ main.tf
@@ -1,30 +1,23 @@
- terraform {
+terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
+      version = "2.16.0"
     }
   }
-   backend "local" {
-    path = "./terraform.tfstate"
-  }
 }
 
-
-
-
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tony_server"
-  ports {
-    internal = 80
-    external = 8000
-  }
+# Pulls the image
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
+}
+
+# Create a container
+resource "docker_container" "foo" {
+  image = docker_image.ubuntu.latest
+  name  = "foo"
 }

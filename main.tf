9eb11fb47c6043012ecd695ba5e6171e600fc9f5
tony <tony@xiatech.co.uk>
2022-02-15 11:40:22 +0100

    working

--- main.tf
+++ main.tf
@@ -12,12 +12,3 @@ provider "docker" {
 }
 
 # Pulls the image
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
-}
-
-# Create a container
-resource "docker_container" "foo" {
-  image = docker_image.ubuntu.latest
-  name  = "foo"
-}

35099c37d12c4950954f3ed803fcfe4942dba05c
tony <tony@xiatech.co.uk>
2022-02-15 11:49:03 +0100

    removing nginx

--- main.tf
+++ main.tf
@@ -12,16 +12,3 @@ provider "docker" {
 }
 
 # Pulls the image
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
